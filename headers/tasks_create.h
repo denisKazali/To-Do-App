891fac4e04dc0b7670d1fe8d24680c9471e30296
denisKazali <d.kazali99@gmail.com>
2025-06-09 17:38:38 +0300

    Added comments to main.c

--- headers/tasks_create.h
+++ headers/tasks_create.h
@@ -1,7 +1,7 @@
 #ifndef TASKS_CREATE_H
 #define TASKS_CREATE_H
-#include "~/headers/file_utils.h"
-#include "~/headers/tasks_type.h"
+#include "../headers/file_utils.h"
+#include "../headers/tasks_type.h"
 
 int create_task(Task* task);
 
--- source/main.c
+++ source/main.c
@@ -3,15 +3,27 @@
 #include "../headers/menu.h"
 #include "../headers/tasks_type.h"
 
-
+//Enum of possible menu choices
 typedef enum{MAIN_MENU, ADD_TASK, LIST_TASKS, DELETE_TASK, EDIT_TASK} menu_options_t;
+//Function to clear the contents of the screen and reset user_option and current_menu variables
 void to_main_menu(int* opt, menu_options_t* mnopt);
 
 int main(void){
+    //On startup
 	clear_menu();
 	print_menu();
-    unsigned short user_option = 5
+    //Creating user_option variable, where we'll store the choice of the user
+    //Setting user_option variable to arbitrary number out of the possible choice range -> 0 to 4
+    //Creating current_menu variable to store the current state of the program
+    //Setting current_menu variable to MAIN_MENU
+    unsigned short user_option = 5;
     menu_options_t current_menu = MAIN_MENU;
+
+    //Main Cycle
+    while(1){
+
+
+    }
 return 0;
 }
 
